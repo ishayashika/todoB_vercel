8b40c31efc89be256e5abd986135e42ef6cf8da1
Yashika <ishayashika@gmail.com>
2025-05-29 21:20:32 +0530

    Updated backend with MongoDB connection

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors({
     'http://localhost:5174',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:5174',
-    'https://your-render-frontend-url.onrender.com' // Add your frontend URL
+    'https://todob-vercel.onrender.com' // Add your frontend URL
   ],
   credentials: true
 })); //helps us to share our info between clients
